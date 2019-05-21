26d5e39e45bf3acde48ca14a9fd4196a36ccea82
Sooram <rkdtnfka@naver.com>
2019-05-21 16:59:01 +0900

    Add files via upload

--- dcgan.py
+++ dcgan.py
@@ -39,56 +39,49 @@ class DCGAN(Model):
         ))
         self.loss_G = loss_G
         
-        t_vars = tf.trainable_variables( ) # return list
+        D_global_step = tf.Variable(0, trainable=False, name='D_global_step')
+        G_global_step = tf.Variable(0, trainable=False, name='G_global_step')
         
-        g_vars = [ var for var in t_vars if "g" in var.name ]
-        d_vars = [ var for var in t_vars if "d" in var.name ]
+        vars_D = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+            scope='discriminator')
+        self.vars_D = vars_D
+        vars_G = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+            scope='generator')
+        self.vars_G = vars_G 
         
-        update_ops = tf.get_collection( tf.GraphKeys.UPDATE_OPS )
-        with tf.control_dependencies( update_ops ) :    
-            train_G = tf.train.AdamOptimizer( config['lr_G'], beta1 = 0.5 ).minimize( loss_G, var_list = g_vars )
-            train_D = tf.train.AdamOptimizer( config['lr_D'], beta1 = 0.5 ).minimize( loss_D, var_list = d_vars )    
-        self.train_G = train_G
-        self.train_D = train_D
-        
-#        D_global_step = tf.Variable(0, trainable=False, name='D_global_step')
-#        G_global_step = tf.Variable(0, trainable=False, name='G_global_step')
-#        
-#        vars_D = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-#            scope='discriminator')
-#        self.vars_D = vars_D
-#        vars_G = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-#            scope='generator')
-#        self.vars_G = vars_G 
-#        
-#        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-#        with tf.control_dependencies(update_ops):
-#            train_D = tf.train.AdamOptimizer(config['lr_D']).minimize(loss_D,
-#                var_list=vars_D, global_step=D_global_step)
-#            self.train_D = train_D
-#            train_G = tf.train.AdamOptimizer(config['lr_G']).minimize(loss_G,
-#                var_list=vars_G, global_step=G_global_step)
-#            self.train_G = train_G
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_D = tf.train.AdamOptimizer(config['lr_D']).minimize(loss_D,
+                var_list=vars_D, global_step=D_global_step)
+            self.train_D = train_D
+            train_G = tf.train.AdamOptimizer(config['lr_G']).minimize(loss_G,
+                var_list=vars_G, global_step=G_global_step)
+            self.train_G = train_G
     
         super(DCGAN, self).__init__(50)
-#        variables_to_save = self.vars_D + self.vars_G + tf.moving_average_variables()
-#        super(DCGAN, self).__init__(variables_to_save, 50)
-        
+
+
+def lrelu(x, leak=0.2):
+    return tf.maximum(x, x * leak)
 
         
 class Generator(object):
     def __call__(self, noise, is_training):
         with tf.variable_scope('generator'):
-            output = tf.layers.dense(noise, 128*9*9*9)
-            output = tf.reshape(output, [-1, 9, 9, 9, 128])
-            output = tf.nn.relu(tf.layers.batch_normalization(output, training=is_training))
+            output = tf.layers.dense(noise, 512*4*4*4)
+            output = tf.reshape(output, [-1, 4, 4, 4, 512])
+            output = lrelu(tf.layers.batch_normalization(output, training=is_training))
+            output = tf.layers.conv3d_transpose(output, 256, [5, 5, 5], strides=(2, 2, 2), padding='SAME')
+            output = lrelu(tf.layers.batch_normalization(output, training=is_training))
+            output = tf.layers.conv3d_transpose(output, 128, [5, 5, 5], strides=(2, 2, 2), padding='SAME')
+            output = lrelu(tf.layers.batch_normalization(output, training=is_training))
             output = tf.layers.conv3d_transpose(output, 64, [5, 5, 5], strides=(2, 2, 2), padding='SAME')
-            output = tf.nn.relu(tf.layers.batch_normalization(output, training=is_training))
-            output = tf.layers.conv3d_transpose(output, 32, [5, 5, 5], strides=(2, 2, 2), padding='SAME')
-            output = tf.nn.relu(tf.layers.batch_normalization(output, training=is_training))
+            output = lrelu(tf.layers.batch_normalization(output, training=is_training))
             output = tf.layers.conv3d_transpose(output, 1, [5, 5, 5], strides=(1, 1, 1), padding='SAME')
-            output = tf.tanh(output)
+            output = tf.nn.sigmoid(output)
             return output
+            
+
 
         
 class Discriminator(object):        
@@ -96,11 +89,13 @@ class Discriminator(object):
         with tf.variable_scope('discriminator') as scope:
             if reuse:
                 scope.reuse_variables()
-            output = tf.layers.conv3d(inputs, filters=32, kernel_size=[5, 5, 5], strides=(2, 2, 2), padding='SAME')
+            output = tf.layers.conv3d(inputs, filters=64, kernel_size=[5, 5, 5], strides=(2, 2, 2), padding='SAME')
             output = self._leaky_relu(output)
-            output = tf.layers.conv3d(output, filters=64, kernel_size=[5, 5, 5], strides=(2, 2, 2), padding='SAME')
-            output = self._leaky_relu(tf.layers.batch_normalization(output, training=is_training))
             output = tf.layers.conv3d(output, filters=128, kernel_size=[5, 5, 5], strides=(2, 2, 2), padding='SAME')
+            output = self._leaky_relu(tf.layers.batch_normalization(output, training=is_training))
+            output = tf.layers.conv3d(output, filters=256, kernel_size=[5, 5, 5], strides=(2, 2, 2), padding='SAME')
+            output = self._leaky_relu(tf.layers.batch_normalization(output, training=is_training))
+            output = tf.layers.conv3d(output, filters=512, kernel_size=[5, 5, 5], strides=(2, 2, 2), padding='SAME')
             output = self._leaky_relu(tf.layers.batch_normalization(output, training=is_training))
             flat = tf.contrib.layers.flatten(output)
             output = tf.layers.dense(flat, 1, activation=None)
